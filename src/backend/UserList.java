2523ab08035a7a14a063f3e41bb07c44e74e3045
Ian Kruger <107899965+ianckruger@users.noreply.github.com>
2024-04-15 21:53:43 +0000

    updating id to make a studnet information accurate list

--- src/backend/UserList.java
+++ src/backend/UserList.java
@@ -53,7 +53,6 @@ public class UserList {
     // MOVING THE ADD IN
 
     public void advisor(int choice, Scanner scanner) {
-        System.out.println(choice+ "advisor choice");
         if (choice == 1) {
                 System.out.println("Enter a student ID to find: ");
                 String studentId = scanner.nextLine();
